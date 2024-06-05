1be8426cfa474fa7ef4a6cc1ad2711e8c0c6fa75
Nolan Flannery <nolanflannery@gmail.com>
2024-06-04 17:30:45 -0700

    added tutorial pages

--- server.js
+++ server.js
@@ -8,9 +8,50 @@ const PORT = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Routes for part files
+app.get('/part1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part1.html'));
+});
+
+app.get('/part2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part2.html'));
+});
+
+app.get('/part3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part3.html'));
+});
+
+app.get('/part4', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part4.html'));
+});
+
+app.get('/part5', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part5.html'));
+});
+
+app.get('/part6', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part6.html'));
+});
+
+app.get('/part7', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part7.html'));
+});
+
+app.get('/part8', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part8.html'));
+});
+
+app.get('/part9', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part9.html'));
+});
+
+app.get('/part10', (req, res) => {
+  res.sendFile(path.join(__dirname, 'part10.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
